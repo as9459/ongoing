2f7194d3e39c6c4263497779b7fe7affd14c280a
mouhssine572 <mohsineb56@gmail.com>
2023-12-06 17:25:38 +0100

    .

--- Gestion locative/Java/src/jdbc/CictOracleDataSource.java
+++ Gestion locative/Java/src/jdbc/CictOracleDataSource.java
@@ -638,7 +638,7 @@ public class CictOracleDataSource extends OracleDataSource {
         }
     }
 
-    public ResultSet GetAllLogements0 () throws SQLException {
+    /*public ResultSet GetAllLogements() throws SQLException {
         try (CallableStatement cs = this.connection.prepareCall("{ ? = call GetAllLogements () }")) {
             cs.registerOutParameter(1, java.sql.Types.REF_CURSOR);
             cs.execute();
@@ -650,7 +650,7 @@ public class CictOracleDataSource extends OracleDataSource {
 
             return rowSet;
         }
-    }
+    }*/
 
 
 
